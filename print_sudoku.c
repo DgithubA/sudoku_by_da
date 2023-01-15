305eef5a04f5ec31414b0d8a9b3bea1478e1afc9
D.A <56259646+DgithubA@users.noreply.github.com>
2023-01-15 18:08:50 +0330

    Update print_sudoku.c

--- print_sudoku.c
+++ print_sudoku.c
@@ -3,46 +3,45 @@
 //
 
 void print_sudoku(int board[9][9]) {
-    HANDLE  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     char hr[] = "  +---+---+---+---+---+---+---+---+---+";
-    SetConsoleTextAttribute(hConsole, dif_color);
+    if(color) MySetTextColor(dif_color);
     printf("    1   2   3   4   5   6   7   8   9");
-    SetConsoleTextAttribute(hConsole, def_wAtt);
+    if(color) MySetTextColor(def_wAtt);
     for (int i = 0; i < 9; i++) {
         if((i % 3) == 0) {
-            SetConsoleTextAttribute(hConsole, dif_color);
+            if(color) MySetTextColor(dif_color);
             printf("\n%s\n",hr);
-            SetConsoleTextAttribute(hConsole, def_wAtt);
+            if(color) MySetTextColor(def_wAtt);
         }else printf("\n%s\n",hr);
-        SetConsoleTextAttribute(hConsole, dif_color);
+        if(color) MySetTextColor(dif_color);
         printf("%d ",i+1);
-        SetConsoleTextAttribute(hConsole, def_wAtt);
+        if(color) MySetTextColor(def_wAtt);
         for (int j = 0; j < 9; j++) {
             if((j % 3) == 0){
-                SetConsoleTextAttribute(hConsole, dif_color);
+                if(color) MySetTextColor(dif_color);
                 printf("|");
-                SetConsoleTextAttribute(hConsole, def_wAtt);
+                if(color) MySetTextColor(def_wAtt);
             }else printf("|");
             if(board[i][j] != 0){
                 if(def_board[i][j] == 0) {//پیشفرض خالیه
                     int prv = board[i][j];
                     board[i][j] = 0;
                     if(!is_valid_input(board , i , j,prv)){
-                        SetConsoleTextAttribute(hConsole, 12);//red
+                        if(color) MySetTextColor(12);//red
                     }else {
-                        SetConsoleTextAttribute(hConsole, 5);//purple
+                        if(color) MySetTextColor(5);//purple
                     }
                     board[i][j] = prv;
                     printf(" %d ", board[i][j]);
-                    SetConsoleTextAttribute(hConsole, def_wAtt);
+                    if(color) MySetTextColor(def_wAtt);
                 }else printf(" %d ", board[i][j]);
             }else printf("   ");
         }
-        SetConsoleTextAttribute(hConsole, dif_color);
+        if(color) MySetTextColor(dif_color);
         printf("|");
-        SetConsoleTextAttribute(hConsole, def_wAtt);
+        if(color) MySetTextColor(def_wAtt);
     }
-    SetConsoleTextAttribute(hConsole, dif_color);
+    if(color) MySetTextColor(dif_color);
     printf("\n%s\n",hr);
-    SetConsoleTextAttribute(hConsole, def_wAtt);
+    if(color) MySetTextColor(def_wAtt);
 }
